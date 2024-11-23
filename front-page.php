dd7fa66232e6cc46a43bfca8801d66ff393d726c
Aiden Roberts <aidenwatsonroberts@gmail.com>
2024-11-23 18:00:47 +0100

    updated front page -- + baselining for CI/CD pipeline

--- front-page.php
+++ front-page.php
@@ -11,31 +11,27 @@
       ?>
     </section>
 
-    <!-- TAG Ukraine Strategy -->
+    <!--SPOTLIGHT-->
+    <div class="section-banner"><h5>Spotlight</h5></div>
+    <div class="section-separator"></div>
     <section class="front-page-section row-theme-dark">
-      <div class="container-lg premium-section">
-        <h1 class="display-5 premium-section-title">[Updated] TAG Ukraine Strategy</h1>
-        <p class="lead"> As Russia's war of aggression enters its second year, this critical TAG update addresses the scope of Western support required to reinforce the goal of the re-estabishment of Ukraine as a secure and sovereign European democracy.</p>
-            <hr>
-            <p class="lead"> Version 2: Released June, 2023 </p>
-            <p class="lead"> Version 1: Released February, 2023 </p>
-              <hr>
-        <div class="row row-eq-height g-2">
-          <?php
-          $UKR_query = new WP_Query( array(
-            "tag" => "UkraineTAG",
-            "posts_per_page" => 2
-            ));
-          if($UKR_query->have_posts()) {
-                    while ($UKR_query->have_posts()) {
-                        $UKR_query->the_post();
-                get_template_part( "template-parts/content", "smallpreview", array("postID" => $post->ID));
-            }
-          }
-          ?>
+      <div class="container spotlight-section">
+        <div class="row">
+          <h1 class="centered"> Report: The Future Defence, Deterrence and Resilience Conference <h1>
+        </div>
+        <div class="row">
+            <p class="lead centered">
+            The Future Defence, Deterrence and Resilience Conference was the third in a trilogy of policy-focussed future war/defence conferences. The 2022 Future War and Deterrence Conference considered defence strategy going forward in an uncertain and strategically competitive world for the Alliance and Partners. The 2023 Future War, Strategy and Technology Conference examined the impact of Emerging and Disruptive Technologies (EDT) on Allied and Partner defence strategy. The Future Defence, Deterrence and Resilience Conference focuses on the balance to be struck between people protection and power projection, civil defence and military defence in the face of the hybrid war in which autocratic powers are already engaged against open, democratic societies. As such, the Conference explored the civil military partnerships that will be vital to affecting such resilience.
+            </p>
+        </div>
+        <div class="row centered">
+          <a href="https://thealphengroup.com/wp-content/uploads/2024/11/WP3395-FDDR-Report.pdf">
+            <button type="button" class="btn btn-outline-primary">Download the report</button>
+          </a>
         </div>
       </div>
-    </section> 
+    </section>
+
 
     <!-- LATEST POST AREA -->
     <div class="section-banner"><h5>Latest Post</h5></div>
@@ -67,67 +63,30 @@
       </div>
     </section>
 
-    <div class="section-banner"><h5> Category Highlight </h5></div>
-    <div class="section-separator"></div>
-
-    <!-- TAG PREMIUM BLOG -->
-    <section class="front-page-section row-theme-dark">
+    <!-- <div class="section-banner"><h5> Category Highlight </h5></div>
+    <div class="section-separator"></div> -->
+  
+    <!-- TAG Russia Strategy -->
+    <section class="front-page-section row-theme-light">
       <div class="container-lg premium-section">
-        <h1 class="display-5 premium-section-title"> TAG Premium Blog </h1>
-        <p class="lead"> Each month, a member of TAG publishes a concise piece on a topic of their choosing. The Premium Blog is editted by Judy Dempsey.</p>
-        <hr>
-
-          <div class="row g-2">
-            <?php
-              $prem_query = new WP_Query( array(
-                "category_name" => "TAG Premium",
-                "posts_per_page" => 4
-              ));
-              $prem_counter = 0;
-              if($prem_query->have_posts()) {
-                while ($prem_query->have_posts()) {
-                  $prem_query->the_post();
-                  if($prem_counter == 0) {
-                    ?> <div class="col"> <?php
-                    get_template_part( "template-parts/content", "premium", array("postID"=> $post->ID));
-                    ?> </div> <?php
-                  } else if($prem_counter == 1) {
-                    ?> <div class="col"> <?php
-                    get_template_part( "template-parts/content", "premiumpreview", array("postID"=> $post->ID));
-                  } else if($prem_counter == 3){
-                    get_template_part( "template-parts/content", "premiumpreview", array("postID"=> $post->ID));
-                    ?> </div> <?php
-                  } else {
-                    get_template_part( "template-parts/content", "premiumpreview", array("postID"=> $post->ID));
-                  }
-                  $prem_counter = $prem_counter + 1;
-                }
-              }
-            ?>
-          </div>
-      </div>
-    </section>
-
-    <!-- SPOTLIGHT-->
-    <div class="section-banner"><h5>Spotlight</h5></div>
-    <div class="section-separator"></div>
-    <section class="front-page-section row-theme-dark">
-      <div class="container spotlight-section">
-        <div class="row">
-          <h1 class="centered">Wilton Park Conference on Future War and Deterrence<h1>
-        </div>
-        <div class="row">
-            <p class="lead centered">
-              Wilton Park and The Alphen Group jointly organised a three day, invitation only conference on future war and deterrence, bringing together over 60 leaders, experts, analysts and commentators from public policy and politics, the armed forces, the private sector, and from technology and innovation. Participants came from the democratic world across North America, Europe and Asia.
-            </p>
-        </div>
-        <div class="row centered">
-          <a href="https://thealphengroup.com/2022/10/31/the-future-war-and-deterrence-conference-report/">
-            <button type="button" class="btn btn-outline-primary">READ MORE</button>
-          </a>
+        <h1 class="display-5 premium-section-title"> TAG Russia Strategy</h1>
+        <p class="lead center"> We are determined to constrain and contest Russia’s aggressive actions and to counter its ability to conduct destabilising activities towards NATO and Allies. For our next Summit, we will develop recommendations on NATO’s strategic approach to Russia, taking into account the changing security environment.
+        <div class="row row-eq-height g-2">
+          <?php
+          $RUSSIA_query = new WP_Query( array(
+            "tag" => "RussiaStrategy",
+            "posts_per_page" => 1
+            ));
+          if($RUSSIA_query->have_posts()) {
+                    while ($RUSSIA_query->have_posts()) {
+                        $RUSSIA_query->the_post();
+                get_template_part( "template-parts/content", "widepreview", array("postID" => $post->ID));
+            }
+          }
+          ?>
         </div>
       </div>
-    </section>
+    </section> 
 
   </div>
 <?php
---
+++ template-parts/content-widepreview.php
@@ -0,0 +1,39 @@
+<?php
+  $postID = $args["postID"];
+?>
+
+<div class="col-md-12
+">
+  <div class="card preview-card">
+    <div class="preview-background">
+      <?php
+      if(has_post_thumbnail($postID)) {
+        echo get_the_post_thumbnail($postID, "full", ['class' => 'preview-background-image']);
+      } else {
+      ?>
+        <img class="preview-background-image" src="<?php echo get_template_directory_uri(); ?>/assets/images/default-background.jpg"/>
+      <?php
+      }
+      ?>
+    </div>
+
+    <div class="card-body nextrecent-card-body">      
+      <?php
+        $category = get_the_category($postID)[0];
+        $output = '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" alt="' . esc_attr( sprintf( __( 'View all posts in %s', 'textdomain' ), $category->name ) ) . '"><div class="metadata nextrecent-post-category"> <i class="far fa-folder"></i> ' . esc_html( $category->name ) . '</div></a>';
+        echo $output;
+      ?>
+      <div class="metadata nextrecent-post-date"> <i class="far fa-calendar"></i> <?php echo get_the_date('', $postID); ?> </div>
+
+      <a href="<?php echo get_permalink($postID) ?>">
+        <h3 class="card-title nextrecent-post-title">
+          <?php
+          echo get_the_title($postID);
+          ?>
+        </h3>
+      </a>
+      <p class="card-subtitle mb-2 nextrecent-post-author"><?php echo get_post_meta($postID, 'author-byline', true); ?></p>
+    
+    </div>
+  </div>
+</div>
