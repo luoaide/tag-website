9d9fb9684cd0980e36e3b838f023bdfa0915c8cc
Aiden Roberts <aidenwatsonroberts@gmail.com>
2025-03-29 13:09:52 +0100

    added TAG Talks section + CI/CD Pipeline

--- TAG_Template/front-page.php
+++ TAG_Template/front-page.php
@@ -17,19 +17,24 @@
     <!-- TAG Atlantic Charter Strategy -->
     <section class="front-page-section row-theme-dark">
       <div class="container-lg premium-section">
-        <h1 class="display-5 premium-section-title">ATLANTIC CHARTER 2025</h1>
-        <h3 class="premium-section-title">A NEW NATO DEAL FOR AMERICA</h3>
-        <p class="lead center premium-section-title"> Atlantic Charter 2025 builds on TAG Transatlantic Compact 2024 by looking beyond the debate over spending 2%, 3% or even 4% of GDP on defense by the European Allies. To that end, the Charter focuses on the <strong> minimum military capabilities, capacities and structures NATO will need to do the job both implicit and explicit in SACEUR’s Family of Plans adopted at the 2023 Vilnius Summit</strong>. The Charter is based on a worst-case analysis of the contingencies and assumptions with which NATO’s defense and deterrence posture could have to contend.
-          <div class="row row-eq-height g-2">
+        <?php
+        $category3 = "TAG Talks";
+        $category_id3 = get_cat_ID($category3);
+        $category_link3 = get_category_link( $category_id3 );
+        ?>
+        <h1 class="display-5 premium-section-title">TAG Talks</h1>
+        <h3 class="premium-section-title">Julian Lindley French Interviews TAG Members</h3>
+        <a href="<?php echo esc_url( $category_link3 ); ?>"><h3 class="lead center premium-section-title"> View all TAG Talks here</h3></a>
+        <div class="row row-eq-height g-2">
           <?php
-          $ATLANTICCHARTER2025_query = new WP_Query( array(
-            "tag" => "AtlanticCharter2025",
-            "posts_per_page" => 1
+          $TAGTALKS_query = new WP_Query( array(
+            "tag" => "TAGTALK",
+            "posts_per_page" => 2
             ));
-          if($ATLANTICCHARTER2025_query->have_posts()) {
-            while ($ATLANTICCHARTER2025_query->have_posts()) {
-                $ATLANTICCHARTER2025_query->the_post();
-                get_template_part( "template-parts/content", "widepreview", array("postID" => $post->ID));
+          if($TAGTALKS_query->have_posts()) {
+            while ($TAGTALKS_query->have_posts()) {
+                $TAGTALKS_query->the_post();
+                get_template_part( "template-parts/content", "smallpreview", array("postID" => $post->ID));
             }
           }
           ?>
@@ -68,9 +73,35 @@
       </div>
     </section>
 
+    <!--SPOTLIGHT-->
     <div class="section-banner"><h5>Spotlight</h5> </div>
     <div class="section-separator"></div>
-  
+    <!-- TAG Atlantic Charter Strategy -->
+    <section class="front-page-section row-theme-dark">
+      <div class="container-lg premium-section">
+        <h1 class="display-5 premium-section-title">ATLANTIC CHARTER 2025</h1>
+        <h3 class="premium-section-title">A NEW NATO DEAL FOR AMERICA</h3>
+        <p class="lead center premium-section-title"> Atlantic Charter 2025 builds on TAG Transatlantic Compact 2024 by looking beyond the debate over spending 2%, 3% or even 4% of GDP on defense by the European Allies. To that end, the Charter focuses on the <strong> minimum military capabilities, capacities and structures NATO will need to do the job both implicit and explicit in SACEUR’s Family of Plans adopted at the 2023 Vilnius Summit</strong>. The Charter is based on a worst-case analysis of the contingencies and assumptions with which NATO’s defense and deterrence posture could have to contend.
+          <div class="row row-eq-height g-2">
+          <?php
+          $ATLANTICCHARTER2025_query = new WP_Query( array(
+            "tag" => "AtlanticCharter2025",
+            "posts_per_page" => 1
+            ));
+          if($ATLANTICCHARTER2025_query->have_posts()) {
+            while ($ATLANTICCHARTER2025_query->have_posts()) {
+                $ATLANTICCHARTER2025_query->the_post();
+                get_template_part( "template-parts/content", "widepreview", array("postID" => $post->ID));
+            }
+          }
+          ?>
+        </div>
+      </div>
+    </section> 
+
+    <div class="section-banner"><h5>Spotlight</h5> </div>
+    <div class="section-separator"></div>
+
     <!-- TAG Russia Strategy -->
     <section class="front-page-section row-theme-light">
       <div class="container-lg premium-section">
